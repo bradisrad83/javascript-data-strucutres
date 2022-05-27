1e6c2d8b662de5f7d710f7eea83383b63f966b89
Brad Goldsmith <bradgoldsmith@Brads-MacBook-Air.local>
2022-05-27 09:44:49 -0400

    fixed insertion sort

--- Algorithms/InsertionSort.js
+++ Algorithms/InsertionSort.js
@@ -1,8 +1,10 @@
 function insertionSort(arr) {
-    for(var i = 1; i < arr.length; i++) {
-        var currentVal = arr[i];
-        for(var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+    for(let i = 1; i < arr.length; i++) {
+        let currentVal = arr[i];
+        let j = i - 1;
+        while(j >= 0 && currentVal < arr[j]) {
             arr[j+1] = arr[j];
+            j--;
         }
         arr[j+1] = currentVal;
     }
